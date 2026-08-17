bcbea780198df9767f7d7b3502c81fdb83b10a89
youssefyouyoudev <contact@youssefyouyou.com>
2026-08-17 12:58:18 +0100

    hh

--- backend/config/app.php
+++ backend/config/app.php
@@ -67,8 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+ 'timezone' => env('APP_TIMEZONE', 'Africa/Casablanca'),
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
